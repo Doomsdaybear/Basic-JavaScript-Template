7ade945f888a4dc51dad49cc1e7d1ef0fbe7f9ae
Doomsdaybear <101878029+Doomsdaybear@users.noreply.github.com>
2022-04-04 08:10:02 -0400

    Create ifstatement.js
    bit harder than the other ones

---
+++ ifstatement.js
@@ -0,0 +1,19 @@
+var choosing = Math.floor(Math.random() * 4)  //Chooses a number between 0-4
+// checks if choosing is equal to 0, then tells the user that
+if(choosing == 0){
+  alert("Your number is 0")
+}
+//we follow this logic 4 more times.
+if(choosing == 1){
+  alert("Your number is 1")
+}
+if(choosing == 2){
+ alert("Your number is 2") 
+}
+if(choosing == 3){
+ alert("Your number is 3") 
+}
+if(choosing == 4) {
+  alert("Your number is 4")
+}
+//And that's how you make a random number generator using if statements and variables!
